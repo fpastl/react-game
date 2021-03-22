666dea9f3be178b0256ab6000c0af835b45e1b5c
fpastl <75983960+fpastl@users.noreply.github.com>
2021-03-22 23:26:27 +0300

    refactor: representation structure

--- src/components/HotKey.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function HotKey(props){
-    const {MenuItem,Key}=props;
-    return(
-        <div className='Row HK'><span className='MenuItem'>{MenuItem+':'}</span><span className='key'>{Key}</span></div>
-    );
-}
--- src/components/ResultRow.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function ResultRow(props){
-    const {data,position}=props;
-    return (
-        <tr><td>{position}</td><td>{data[0]}</td><td>{data[1]}</td></tr>
-    )
-}
